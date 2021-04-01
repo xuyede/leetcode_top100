53d9c0b35f3a8f2507c9cd9f4ade281ff11e2bb2
xuyede <yede.xu@jyblife.com>
2021-04-01 19:53:36 +0800

    4-1

--- index.js
+++ index.js
@@ -1,39 +1,28 @@
 // https://leetcode-cn.com/problemset/leetcode-hot-100/
-// https://leetcode-cn.com/problems/generate-parentheses/solution/pei-yang-chou-xiang-si-wei-hui-su-jie-fa-7dwu/
 
-// 括号生成
+// 最长有效括号
 /**
-  数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+  给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号子串的长度
 
-  输入：n = 3
-  输出：["((()))","(()())","(())()","()(())","()()()"]
+  输入：s = "(()"
+  输出：2
+  解释：最长有效括号子串是 "()"
+  示例 2：
 
-  输入：n = 1
-  输出：["()"]
+  输入：s = ")()())"
+  输出：4
+  解释：最长有效括号子串是 "()()"
+  示例 3：
+
+  输入：s = ""
+  输出：0
 */
 
 
-const generateParenthesis = function(n) {
-  const res = [];
-  dfs(n, '', 0, 0, res);
-
-  // open: 左括号的数量 close：右括号的数量
-  function dfs(n, path, open, close, res) {
-    if (open > n || close > n || close > open) {
-      return;
-    }
-    if (path.length >= 2 * n) {
-      res.push(path);
-      return;
-    }
-
-    dfs(n, path + '(', open + 1, close, res);
-    dfs(n, path + ')', open, close + 1, res);
-  }
-
-  return res;
+const longestValidParentheses = function(s) {
+  
 }
 
 console.log(
-  generateParenthesis(4)
+  longestValidParentheses('(())()')
 )
